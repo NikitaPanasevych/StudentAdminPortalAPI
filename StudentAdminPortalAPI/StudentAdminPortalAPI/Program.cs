904a2f56931304afdfd71b65627f7d3af151b740
DeStRaaM <20destraam03@gmail.com>
2023-06-28 20:34:39 +0200

    Added CORS policy for frontend.

--- StudentAdminPortalAPI/StudentAdminPortalAPI/Program.cs
+++ StudentAdminPortalAPI/StudentAdminPortalAPI/Program.cs
@@ -6,6 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors((options) =>
+{
+    options.AddPolicy("angularUI", (builder) =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .WithMethods("GET", "POST", "PUT", "DELETE")
+        .WithExposedHeaders("*");
+    });
+});
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StudentAdminContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("StudentAdminPortalDb")
@@ -27,6 +37,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("angularUI");
 
 app.UseAuthorization();
 
